328f196496c61fe111a6e90c8deac11f410c3dc5
thejsj <jorge.silva@thejsj.com>
2015-02-12 12:12:03 -0800

    Add fixes to homepage slideshow

--- js/getAppropriateSize.js
+++ js/getAppropriateSize.js
@@ -6,17 +6,19 @@
   var $window = $(window);
   window.getAppropriateSize = (function () {
     return function (index) {
+      var patt = new RegExp(/([0-9]*)x([0-9]*)([.A-z]*)$/gi);
       index = (index !== undefined ? index : 0);
       if (window.CaitlinCordtzData !== undefined) {
         var maxWindowLength = Math.max($window.height(), $window.width());
         var allSizes = window.CaitlinCordtzData.images[index].sizes;
         var sizesLargerThanMaxWindowLength = _.filter(allSizes, function (imageName, key) {
           if (key.indexOf('size') > -1) {
-            var patt = new RegExp(/([0-9]*)x([0-9]*)([.A-z]*)$/gi);
             var _sizes = patt.exec(imageName);
-            var size = Math.min(_sizes[1], _sizes[2]);
-            if (size > maxWindowLength) {
-              return true;
+            if (_.isArray(_sizes) && _sizes.length >= 2) {
+              var size = Math.min(_sizes[1], _sizes[2]);
+              if (size > maxWindowLength) {
+                return true;
+              }
             }
           }
           return false;
